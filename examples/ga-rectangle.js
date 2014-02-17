17e5118a339987cd10d605364eb5ad355a504404
Stefan Reist <stefan.reist@swisstopo.ch>
2014-02-17 09:20:02 +0100

    modified rectangle

--- examples/ga-rectangle.js
+++ examples/ga-rectangle.js
@@ -1,31 +1,54 @@
 window.init = function() {
+ 
+      var layer_0 = ga.layer.create('ch.swisstopo.pixelkarte-farbe');
+      var layer_1 = ga.layer.create('ch.swisstopo.pixelkarte-farbe-pk25.noscale');
+      var layer_2 = ga.layer.create('ch.swisstopo.pixelkarte-farbe-pk50.noscale');
+      var layer_3 = ga.layer.create('ch.swisstopo.pixelkarte-farbe-pk100.noscale');
+      var layer_4 = ga.layer.create('ch.swisstopo.pixelkarte-farbe-pk200.noscale');
+      var layer_5 = ga.layer.create('ch.swisstopo.pixelkarte-farbe-pk500.noscale');
 
-      var layer = ga.layer.create('ch.swisstopo.pixelkarte-farbe');
+      var layers = [layer_0, layer_1, layer_2, layer_3, layer_4, layer_5];
+
+
       var map = new ga.Map({
         target: 'map',
-        layers: [layer],
+        layers: layers,
         view: new ol.View2D({
           resolution: 500,
-          center: [670000, 160000]
+          center: [670000, 160000],
+          zoom: 2    
         })
-      });
+      });      
 
-    var defaultStyle =  new ol.style.Style({
-      fill: new ol.style.Fill({
+
+
+      var zoomslider = new ol.control.ZoomSlider();
+      map.addControl(zoomslider);
+
+      var setOneLayerVisible = function(layerIndex) {
+        for (var i = 0, ii = layers.length; i < ii; i++) {
+          var layer = layers[i];
+          if (i == layerIndex) {
+            layer.setVisible(true);  
+          } else {
+            layer.setVisible(false);
+          }
+        }
+      } 
+      
+      // Makes the layer_0 visible
+      setOneLayerVisible(0); 
+       
+
+      var defaultStyle =  new ol.style.Style({
+        fill: new ol.style.Fill({
           color: 'rgba(255, 0, 0, 0.3)'
-          }),
-      stroke:
-	new ol.style.Stroke({ 
+        }),
+        stroke: new ol.style.Stroke({ 
           color: '#FF0000',
           width:2
-         }) 
-       });
-
-
-       $("#button").click(function(e){
-            e.preventDefault();
-            map.addInteraction(dragBox);
-          });
+        }) 
+      });
 
 
       var dragBox = new ol.interaction.DragBox({
@@ -51,9 +74,6 @@ window.init = function() {
        });
 
 
-
-
-
       var feature = new ol.Feature();
 	  
       var overlay = new ol.render.FeaturesOverlay({
@@ -73,15 +93,24 @@ window.init = function() {
                 
       dragBox.on('boxend', function(evt) {
 	 endCoordinate = evt.getCoordinate();
-          $("#coordinate_west").val(Math.round(startCoordinate[0]));
-          $("#coordinate_nord").val(Math.round(startCoordinate[1]));
-          $("#coordinate_ost").val(Math.round(endCoordinate[0]));
-          $("#coordinate_sued").val(Math.round(endCoordinate[1]));
+          $("#coordinate_west").val(Math.round(startCoordinate[0])).change();
+          $("#coordinate_nord").val(Math.round(startCoordinate[1])).change();
+          $("#coordinate_ost").val(Math.round(endCoordinate[0])).change();
+          $("#coordinate_sued").val(Math.round(endCoordinate[1])).change();
        // alert("coordinates: west: "+startCoordinate[0]+", Sued: " + startCoordinate[1]);
 	// alert("coordinates: Ost: "+endCoordinate[0]+", Nord: " + endCoordinate[1]);
       	 feature.setGeometry(dragBox.getGeometry());
          overlay.addFeature(feature);
          map.removeInteraction(dragBox);
-})
 
+      });
+
+      $("#button").click(function(e){
+        e.preventDefault();
+        map.addInteraction(dragBox);
+      });
+
+      $("[name=Kartenlayer]").click(function() {
+         setOneLayerVisible(this.value);        
+      });
 }
